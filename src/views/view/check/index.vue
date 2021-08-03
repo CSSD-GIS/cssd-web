5d969afbb3ade08a41f1a0f80e178d7d961591a8
FengHetang <1994975650@qq.com>
2021-08-03 16:52:48 +0800

    课堂检测websocket

--- src/views/view/check/index.vue
+++ src/views/view/check/index.vue
@@ -10,47 +10,44 @@
   </div>
 </template>
 
-
-
 <script>
 export default {
   data() {
     this.webSocket()
     return {
-      img:'',
+      img: ''
     }
   },
   mounted: function() {
   },
   methods: {
     webSocket() {
-      const _this = this;
+      const _this = this
       if (typeof (WebSocket) == 'undefined') {
         this.$notify({
           title: '提示',
           message: '当前浏览器无法接收实时报警信息，请使用谷歌浏览器！',
           type: 'warning',
           duration: 0
-        });
+        })
       } else {
         // 实例化socket
-        const socketUrl = 'ws://127.0.0.1:8000/img/';
-        this.socket = new WebSocket(socketUrl);
+        const socketUrl = 'ws://127.0.0.1:8000/img/'
+        this.socket = new WebSocket(socketUrl)
         // 监听socket打开
         this.socket.onopen = function() {
-          console.log('浏览器WebSocket已打开');
-          //发送字符:
+          console.log('浏览器WebSocket已打开')
           _this.socket.send(JSON.stringify({
-            'code': "200",
-            'msg': "wesocket已打开",
-          }));
-        };
+            'code': '200',
+            'msg': 'wesocket已打开'
+          }))
+        }
         // 监听socket消息接收
         this.socket.onmessage = function(msg) {
           // 追加到内容显示列表中
           var content = msg.data
-          _this.img = "data:image/jpg;base64,"+ content
-        };
+          _this.img = 'data:image/jpg;base64,' + content
+        }
         // 监听socket错误
         this.socket.onerror = function() {
           _this.$notify({
@@ -58,14 +55,14 @@ export default {
             message: '服务器错误，无法接收实时报警信息',
             type: 'error',
             duration: 0
-          });
-        };
+          })
+        }
         // 监听socket关闭
         this.socket.onclose = function() {
-          console.log('WebSocket已关闭');
+          console.log('WebSocket已关闭')
         }
       }
-    },
+    }
   }
 }
 </script>
