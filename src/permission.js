e3e3b1bdb91e599a31e21919e80f9f102c354499
FengHetang <1994975650@qq.com>
2021-08-22 17:23:37 +0800

    登录修改

--- src/permission.js
+++ src/permission.js
@@ -1,65 +1,81 @@
 import router from './router'
 import store from './store'
-import { Message } from 'element-ui'
+// import { Message } from 'element-ui'
 import NProgress from 'nprogress' // progress bar
 import 'nprogress/nprogress.css' // progress bar style
-import { getToken } from '@/utils/auth' // get token from cookie
-import getPageTitle from '@/utils/get-page-title'
+import { getToken } from '@/utils/auth'
+import axios from 'axios';
+import { error } from 'autoprefixer/lib/utils' // get token from cookie
+// import getPageTitle from '@/utils/get-page-title'
 
 NProgress.configure({ showSpinner: false }) // NProgress Configuration
 
-const whiteList = ['/login'] // no redirect whitelist
+// const whiteList = ['/login'] // no redirect whitelist
 
 router.beforeEach(async(to, from, next) => {
-    // start progress bar
-    NProgress.start()
-
-    // set page title
-    // document.title = getPageTitle(to.meta.title)
-
-    // determine whether the user has logged in
-    const hasToken = getToken()
-
-    if (hasToken) {
-        if (to.path === '/login') {
-            // if is logged in, redirect to the home page
-            next({ path: '/' })
-            NProgress.done()
-        } else {
-            const hasGetUserInfo = store.getters.name
-            if (hasGetUserInfo) {
-                next()
-            } else {
-                try {
-                    // get user info
-                    await store.dispatch('user/getInfo')
-
-                    next()
-                } catch (error) {
-                    // remove token and go to login page to re-login
-                    await store.dispatch('user/resetToken')
-                    Message.error(error || 'Has Error')
-                    next(`/login?redirect=${to.path}`)
-                    NProgress.done()
-                }
-            }
+  // start progress bar
+  NProgress.start()
+  const hasToken = getToken()
+  if (hasToken) {
+    if (to.path === '/') {
+      axios.get('http://127.0.0.1:8080/api/v1/tet'
+      ).then((response) => {
+          console.log(response)
+          next({ path: '/main' })
+          NProgress.done()
         }
+      ).catch((error) => {
+        console.log(error)
+        if (to.path === '/') {
+          next()
+          NProgress.done()
+        } else {
+          next('/')
+          NProgress.done()
+        }
+      })
     } else {
-        /* has no token*/
-        next()
-            //   if (whiteList.indexOf(to.path) !== -1) {
-            //     // in the free login whitelist, go directly
-            //     next()
-            //   } else {
-            //     // other pages that do not have permission to access are redirected to the login page.
-            //     next(`/login?redirect=${to.path}`)
-            //     NProgress.done()
-            //   }
-            // 
+      axios.get('http://127.0.0.1:8080/api/v1/test'
+      ).then((response) => {
+        console.log(response)
+        NProgress.done()
+        if (to.path === '/') {
+          // if is logged in, redirect to the home page
+          next({ path: '/main' })
+          NProgress.done()
+        } else {
+          const hasGetUserInfo = store.getters.name
+          if (hasGetUserInfo) {
+            next()
+          } else {
+            // try {
+            //   // get user info
+            //   // await store.dispatch('user/getInfo')
+            //   next()
+            // } catch (error) {
+            //   remove token and go to login page to re-login
+            //   await store.dispatch('user/resetToken')
+            //   Message.error(error || 'Has Error')
+            //   next(`/login?redirect=${to.path}`)
+            // }
+            NProgress.done()
+          }
+        }
+      }).catch((error) => {
+        console.log(error)
+        if (to.path === '/') {
+          next()
+          NProgress.done()
+        } else {
+          next('/')
+          NProgress.done()
+        }
+      })
     }
-})
 
-router.afterEach(() => {
-    // finish progress bar
-    NProgress.done()
+    router.afterEach(() => {
+      // finish progress bar
+      NProgress.done()
+    })
+  }
 })
