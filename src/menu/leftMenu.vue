cff146a95f539bc5485e7d278347f0c174e45444
FengHetang <1994975650@qq.com>
2021-08-29 03:11:23 +0800

    代码格式修改

--- src/menu/leftMenu.vue
+++ src/menu/leftMenu.vue
@@ -1,55 +1,55 @@
 <!-- src/menu/leftMenu.vue -->
 <template>
-  <el-menu :collapse="isCollapse" id="left-menu" class="el-menu-vertical" >
+  <el-menu id="left-menu" class="el-menu-vertical" :collapse="isCollapse">
     <!-- @open="handleOpen"   -->
-    <i class="el-icon-s-fold" @click="isC"></i>
+    <i class="el-icon-s-fold" @click="isC" />
     <div class=".el-tooltip__popper.is-dark">
       <el-menu-item class="left-menu-item" index="1">
-        <i class="el-icon-user-solid"></i>
-        <span class="text" slot="title">首页</span>
+        <i class="el-icon-user-solid" />
+        <span slot="title" class="text">首页</span>
       </el-menu-item>
     </div>
     <router-link to="/views/class">
       <el-menu-item index="2">
-        <i class="el-icon-camera"></i>
-        <span class="text" slot="title">视频画面</span>
-        <router-view name="Class"></router-view>
+        <i class="el-icon-camera" />
+        <span slot="title" class="text">视频画面</span>
+        <router-view name="Class" />
       </el-menu-item>
     </router-link>
-     <router-link :to="{ path: '/views/realTime', query: { type: 'null' } }">
+    <router-link :to="{ path: '/views/realTime', query: { type: 'null' } }">
       <el-submenu index="1">
         <template slot="title">
-          <i class="el-icon-folder-remove"></i>
-          <span class="text" slot="title">实时画面</span>
+          <i class="el-icon-folder-remove" />
+          <span slot="title" class="text">实时画面</span>
         </template>
         <router-link :to="{ path: '/views/realTime', query: { type: 'one' } }">
           <el-menu-item index="1-1">
-            <i class="el-icon-location"></i>天仪楼一层
+            <i class="el-icon-location" />天仪楼一层
           </el-menu-item>
         </router-link>
         <router-link :to="{ path: '/views/realTime', query: { type: 'two' } }">
           <el-menu-item index="1-2">
-            <i class="el-icon-location"></i>天仪楼二层
+            <i class="el-icon-location" />天仪楼二层
           </el-menu-item>
         </router-link>
         <router-link :to="{ path: '/views/realTime', query: { type: 'three' } }">
           <el-menu-item index="1-3">
-            <i class="el-icon-location"></i>天仪楼三层
+            <i class="el-icon-location" />天仪楼三层
           </el-menu-item>
         </router-link>
         <router-link :to="{ path: '/views/realTime', query: { type: 'four' } }">
           <el-menu-item index="1-4">
-            <i class="el-icon-location"></i>天仪楼四层
+            <i class="el-icon-location" />天仪楼四层
           </el-menu-item>
         </router-link>
         <router-link :to="{ path: '/views/realTime', query: { type: 'five' } }">
           <el-menu-item index="1-5">
-            <i class="el-icon-location"></i>天仪楼五层
+            <i class="el-icon-location" />天仪楼五层
           </el-menu-item>
         </router-link>
         <router-link :to="{ path: '/views/realTime', query: { type: 'six' } }">
           <el-menu-item index="1-6">
-            <i class="el-icon-location"></i>天仪楼六层
+            <i class="el-icon-location" />天仪楼六层
           </el-menu-item>
         </router-link>
       </el-submenu>
@@ -57,55 +57,53 @@
     <router-link :to="{ path: '/views/floorOne', query: { id: 'null' } }">
       <el-submenu index="3">
         <template slot="title">
-          <i class="el-icon-folder-remove"></i>
-          <span class="text" slot="title">检测画面</span>
+          <i class="el-icon-folder-remove" />
+          <span slot="title" class="text">检测画面</span>
         </template>
         <router-link :to="{ path: '/views/floorOne', query: { id: 'one' } }">
           <el-menu-item index="3-1">
-            <i class="el-icon-location"></i>天仪楼一层
+            <i class="el-icon-location" />天仪楼一层
           </el-menu-item>
         </router-link>
         <router-link :to="{ path: '/views/floorOne', query: { id: 'two' } }">
           <el-menu-item index="3-2">
-            <i class="el-icon-location"></i>天仪楼二层
+            <i class="el-icon-location" />天仪楼二层
           </el-menu-item>
         </router-link>
         <router-link :to="{ path: '/views/floorOne', query: { id: 'three' } }">
           <el-menu-item index="3-3">
-            <i class="el-icon-location"></i>天仪楼三层
+            <i class="el-icon-location" />天仪楼三层
           </el-menu-item>
         </router-link>
         <router-link :to="{ path: '/views/floorOne', query: { id: 'four' } }">
           <el-menu-item index="3-4">
-            <i class="el-icon-location"></i>天仪楼四层
+            <i class="el-icon-location" />天仪楼四层
           </el-menu-item>
         </router-link>
         <router-link :to="{ path: '/views/floorOne', query: { id: 'five' } }">
           <el-menu-item index="3-5">
-            <i class="el-icon-location"></i>天仪楼五层
+            <i class="el-icon-location" />天仪楼五层
           </el-menu-item>
         </router-link>
         <router-link :to="{ path: '/views/floorOne', query: { id: 'six' } }">
           <el-menu-item index="3-6">
-            <i class="el-icon-location"></i>天仪楼六层
+            <i class="el-icon-location" />天仪楼六层
           </el-menu-item>
         </router-link>
       </el-submenu>
     </router-link>
     <el-submenu index="5">
       <template slot="title">
-        <i class="el-icon-folder-remove"></i>
-        <span class="text" slot="title">文件上传</span>
+        <i class="el-icon-folder-remove" />
+        <span slot="title" class="text">文件上传</span>
       </template>
       <router-link to="/views/img">
         <el-menu-item index="4-1">
-          <i class="el-icon-money"></i>图片</el-menu-item
-        >
+          <i class="el-icon-money" />图片</el-menu-item>
       </router-link>
       <router-link to="/views/video">
         <el-menu-item index="4-2">
-          <i class="el-icon-video-camera-solid"></i>视频</el-menu-item
-        >
+          <i class="el-icon-video-camera-solid" />视频</el-menu-item>
       </router-link>
     </el-submenu>
   </el-menu>
@@ -192,7 +190,7 @@ export default {
   list-style: none;
   position: relative;
   margin: 0;
-  padding-left: 10 ;
+  padding-left: 10px ;
   // background-color: rgb(182, 58, 165);
 }
 .text {
