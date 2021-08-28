1d7ef09543dfec00fd2585560973a2fb98ca0b08
liruiyi668 <abcd669_889@163.com>
2021-08-28 20:51:54 +0800

    lry

--- src/menu/leftMenu.vue
+++ src/menu/leftMenu.vue
@@ -1,12 +1,13 @@
 <!-- src/menu/leftMenu.vue -->
 <template>
   <el-menu :collapse="isCollapse" class="el-menu-vertical" id="left-menu">
+    <!-- @open="handleOpen"   -->
     <i class="el-icon-s-fold" @click="isC"></i>
     <div class=".el-tooltip__popper.is-dark">
-    <el-menu-item class="left-menu-item" index="1">
-      <i class="el-icon-user-solid"></i>
-      <span class="text" slot="title">首页</span>
-    </el-menu-item>
+      <el-menu-item class="left-menu-item" index="1">
+        <i class="el-icon-user-solid"></i>
+        <span class="text" slot="title">首页</span>
+      </el-menu-item>
     </div>
     <router-link to="/views/class">
       <el-menu-item index="2">
@@ -15,15 +16,83 @@
         <router-view name="Class"></router-view>
       </el-menu-item>
     </router-link>
-    <router-link to="/views/test">
-      <el-menu-item index="3">
-        <i class="el-icon-video-camera"></i>
-        <span class="text" slot="title">检测画面</span>
-        <router-view name="Test"></router-view>
-      </el-menu-item>
+     <router-link :to="{ path: '/views/realTime', query: { type: 'null' } }">
+      <el-submenu index="1">
+        <template slot="title">
+          <i class="el-icon-folder-remove"></i>
+          <span class="text" slot="title">实时画面</span>
+        </template>
+        <router-link :to="{ path: '/views/realTime', query: { type: 'one' } }">
+          <el-menu-item index="1-1">
+            <i class="el-icon-location"></i>天仪楼一层
+          </el-menu-item>
+        </router-link>
+        <router-link :to="{ path: '/views/realTime', query: { type: 'two' } }">
+          <el-menu-item index="1-2">
+            <i class="el-icon-location"></i>天仪楼二层
+          </el-menu-item>
+        </router-link>
+        <router-link :to="{ path: '/views/realTime', query: { type: 'three' } }">
+          <el-menu-item index="1-3">
+            <i class="el-icon-location"></i>天仪楼三层
+          </el-menu-item>
+        </router-link>
+        <router-link :to="{ path: '/views/realTime', query: { type: 'four' } }">
+          <el-menu-item index="1-4">
+            <i class="el-icon-location"></i>天仪楼四层
+          </el-menu-item>
+        </router-link>
+        <router-link :to="{ path: '/views/realTime', query: { type: 'five' } }">
+          <el-menu-item index="1-5">
+            <i class="el-icon-location"></i>天仪楼五层
+          </el-menu-item>
+        </router-link>
+        <router-link :to="{ path: '/views/realTime', query: { type: 'six' } }">
+          <el-menu-item index="1-6">
+            <i class="el-icon-location"></i>天仪楼六层
+          </el-menu-item>
+        </router-link>
+      </el-submenu>
     </router-link>
-
-    <el-submenu index="4">
+    <router-link :to="{ path: '/views/floorOne', query: { id: 'null' } }">
+      <el-submenu index="3">
+        <template slot="title">
+          <i class="el-icon-folder-remove"></i>
+          <span class="text" slot="title">检测画面</span>
+        </template>
+        <router-link :to="{ path: '/views/floorOne', query: { id: 'one' } }">
+          <el-menu-item index="3-1">
+            <i class="el-icon-location"></i>天仪楼一层
+          </el-menu-item>
+        </router-link>
+        <router-link :to="{ path: '/views/floorOne', query: { id: 'two' } }">
+          <el-menu-item index="3-2">
+            <i class="el-icon-location"></i>天仪楼二层
+          </el-menu-item>
+        </router-link>
+        <router-link :to="{ path: '/views/floorOne', query: { id: 'three' } }">
+          <el-menu-item index="3-3">
+            <i class="el-icon-location"></i>天仪楼三层
+          </el-menu-item>
+        </router-link>
+        <router-link :to="{ path: '/views/floorOne', query: { id: 'four' } }">
+          <el-menu-item index="3-4">
+            <i class="el-icon-location"></i>天仪楼四层
+          </el-menu-item>
+        </router-link>
+        <router-link :to="{ path: '/views/floorOne', query: { id: 'five' } }">
+          <el-menu-item index="3-5">
+            <i class="el-icon-location"></i>天仪楼五层
+          </el-menu-item>
+        </router-link>
+        <router-link :to="{ path: '/views/floorOne', query: { id: 'six' } }">
+          <el-menu-item index="3-6">
+            <i class="el-icon-location"></i>天仪楼六层
+          </el-menu-item>
+        </router-link>
+      </el-submenu>
+    </router-link>
+    <el-submenu index="5">
       <template slot="title">
         <i class="el-icon-folder-remove"></i>
         <span class="text" slot="title">文件上传</span>
@@ -39,30 +108,6 @@
         >
       </router-link>
     </el-submenu>
-    <!-- 第二版菜单栏代码 -->
-    <!--  <el-submenu index="3">
-        <template slot="title">
-          <i class="el-icon-s-claim"></i>
-          <span slot="title">检测画面</span>
-        </template>
-         <router-link to="/t/1"></router-link> -->
-    <!-- <el-submenu index="4">
-          <template slot="title">
-            <i class="el-icon-location"></i>
-            <span slot="title">一层</span>
-          </template>
-          <el-menu-item v-for="(val, key, i) in items1" :key="i">
-            <router-link :to="{ name: 't', query: { id: i } }">{{
-              `${key} `
-            }}</router-link>
-          </el-menu-item>
-        </el-submenu>
-        <el-menu-item>二层</el-menu-item>
-        <el-menu-item>三层</el-menu-item>
-        <el-menu-item>四层</el-menu-item>
-        <el-menu-item>五层</el-menu-item>
-        <el-menu-item>六层</el-menu-item>
-      </el-submenu> -->
   </el-menu>
 </template>
 <script>
@@ -71,7 +116,8 @@ export default {
   name: "LeftMenu",
   data() {
     return {
-      isCollapse: true,
+      id: 0,
+      isCollapse: false,
       items1: config.north.front.floor1,
       items2: config.north.front.floor2,
       items3: config.north.front.floor3,
@@ -90,6 +136,17 @@ export default {
     isC() {
       this.isCollapse = !this.isCollapse;
     },
+    //     handleOpen(index){
+    // if (index=="3-1") {
+    //   let num =items1;
+    //   console.log(num);
+    //   console.log(items1);
+    // }else if (index=="3-2") {
+    //   let num =items2;
+    //   //  console.log(num);
+    //   // console.log(items2);
+    // }
+    //     },
   },
 };
 </script>
@@ -110,9 +167,9 @@ export default {
   -moz-osx-font-smoothing: grayscale;
 }
 .el-tooltip__popper.is-dark {
-     background: #f6f8fa !important; 
-    
-    color: #FFF;
+  background: #f6f8fa !important;
+
+  color: #fff;
 }
 .el-menu-vertical:not(.el-menu--collapse) {
   width: 300px;
@@ -120,18 +177,26 @@ export default {
 
 #left-menu {
   height: 100%;
-  background-color: #f1f1f3;
+  background-color: #e3e3ea;
 }
-.left-menu-item {
+.el-menu {
   border-right: solid 1px #e6e6e6;
   list-style: none;
   position: relative;
   margin: 0;
+  padding-left: 0;
+  background-color: #eef4f9;
+}
+.left-menu-item {
+  // border-right: solid 1px #8f2929;
+  list-style: none;
+  position: relative;
+  margin: 0;
   padding-left: 10;
-  background-color: rgb(245, 242, 238);
+  // background-color: rgb(182, 58, 165);
 }
 .text {
   font-size: 20px;
-  color: #1b1a1a;
+  color: #6394ef;
 }
 </style>
